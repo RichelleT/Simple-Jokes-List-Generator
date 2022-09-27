ce989211819df5135854c106a6a7e7c190a01347
Richelle Tan <r.kyj89@gmail.com>
2022-09-27 15:58:49 +0800

    added assets folder

--- assessment-fd/src/App.js
+++ assessment-fd/src/App.js
@@ -1,7 +1,9 @@
-function App() {
+//import Header from "./components/header";
+
+function  App() {
   return (
-    <div className="App">
-      <h1>Hello World.</h1>
+    <div className='container'>
+      {/* <Header /> */} 
     </div>
   );
 }
---
+++ assessment-fd/src/components/header.js
@@ -0,0 +1,26 @@
+// import React from 'react'
+//import PropTypes from 'prop-types'
+
+/* 
+TODO => create universal navbar/header w/ username 
+TODO => favourite list button at joke list page
+TODO => joke list button on favourite list page 
+*/
+
+const Header = ({title}) => {
+  return (
+    <header>
+        <h1> {title}</h1>
+    </header>
+  )
+}
+
+Header.defaultProps = {
+    title: 'Jokes List Generator',
+}
+
+/* Header.propTypes = {
+    title: PropTypes.string.isRequired,
+} */
+
+export default Header
