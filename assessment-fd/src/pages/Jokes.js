5df4f753c4d7593037406af07af52475745d1054
Richelle Tan <r.kyj89@gmail.com>
2022-09-29 06:04:41 +0800

    save point 6?

--- assessment-fd/src/pages/Jokes.js
+++ assessment-fd/src/pages/Jokes.js
@@ -1,7 +1,59 @@
 import * as React from 'react';
 import Navbar from '../components/Navbar'
+import axios from 'axios';
 
 function Jokes() {
+
+    const [anyJoke, setAnyJoke] = React.useState([])
+    const [puns, setPuns] = React.useState([])
+    const [darkJokes, setDarkJokes] = React.useState([])
+    const [programmingJokes, setProgrammingJokes] = React.useState([])
+
+    const fetchData = () =>{
+        const anyJokeURL ='https://v2.jokeapi.dev/joke/Any?amount=10"'
+        const punsURL ='https://v2.jokeapi.dev/joke/Pun?amount=10'
+        const darkJokesURL ='https://v2.jokeapi.dev/joke/Dark?amount=10'
+        const programmingJokesURL ='https://v2.jokeapi.dev/joke/Programming?amount=10'
+
+        const getAny = axios.get(anyJokeURL)
+        const getPuns = axios.get(punsURL)
+        const getDark = axios.get(darkJokesURL)
+        const getProg = axios.get(programmingJokesURL)
+
+        axios.all([getAny, getPuns, getDark, getProg]). then(
+            axios.spread((...allData) => {
+                const allAny = allData[0].jokes
+                const allPuns = allData[0].jokes
+                const allDark = allData[0].jokes 
+                const allProg = allData[0].jokes 
+
+                setAnyJoke(allAny)
+                setPuns(allPuns)
+                setDarkJokes(allDark)
+                setProgrammingJokes(allProg)
+
+                console.log(allAny)
+            })
+        )
+    }
+
+    React.useEffect(()=>{
+        fetchData()
+    }, [])
+
+/*         axios.all([getAny, getPuns, getDark, getProg]). then(
+            axios.spread((...allData)) => {
+                const allAny = allData[0].jokes
+                const allPuns = allData[0].okes
+            })
+        )
+    } */
+
+
+/*    const anyJoke ='https://v2.jokeapi.dev/joke/Any?amount=10"'
+   const puns ='https://v2.jokeapi.dev/joke/Pun?amount=10'
+   const darkJokes ='https://v2.jokeapi.dev/joke/Dark?amount=10'
+   const programmingJokes ='https://v2.jokeapi.dev/joke/Programming?amount=10' */
 
 /*     const urls = [
         "https://v2.jokeapi.dev/joke/Any?amount=10", 
