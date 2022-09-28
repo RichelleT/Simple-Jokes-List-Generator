38f6600d4ee15e158144ade7f30a1dd0e6369b70
Richelle Tan <r.kyj89@gmail.com>
2022-09-28 23:22:52 +0800

    save 4

--- assessment-fd/src/pages/Home.js
+++ assessment-fd/src/pages/Home.js
@@ -1,11 +1,43 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 //import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 import '../styles/Home.css';
 import BannerImage from '../assets/MoonlitAsteroid.jpg'
-import Jokes from "./Jokes";
+//import Jokes from "./Jokes";
 
-const Home = () => {    
+class Home extends React.Component {
+    
+    constructor(props) {
+      super(props);
+      this.state = {value: ''};
+  
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit(event) {
+      console.log('Hello, ' + this.state.value);
+      event.preventDefault();
+    }
+  
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit"/>
+        </form>
+      );
+    }
+  }
+
+/* const Home = () => {    
     var landingImage = require('../assets/signin.png');
 
     let navigate = useNavigate();
@@ -14,7 +46,7 @@ const Home = () => {
         username: ""
       })
     
-    const [username, setUsername] = useState("")
+    //const [username, setUsername] = useState("")
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -35,8 +67,9 @@ const Home = () => {
                         <form onSubmit={handleSubmit}>
                             <label htmlFor='username'>Name</label>
                             <input type="text" onChange={(e) => setFormData({...formData, username: e.target.value})} name="username" id="username" placeholder="Enter Name" /><br/><br/>
-                            <button className='signin-button' style={{marginLeft:"auto", position:"absolute"}} type='submit'>Sign In</button> <br/> 
+                            <button className='signin-button' onSubmit={handleSubmit} style={{marginLeft:"auto", position:"absolute"}} type='submit'>Sign In</button> <br/> 
                         </form> 
+                        <h1>{formData.username}</h1>
                     </div>
                 </div>
                 <div className='imgCol col-6'>
@@ -46,6 +79,6 @@ const Home = () => {
         </div>
     </div>
   )
-} 
+}  */
 
 export default Home
--- assessment-fd/src/pages/Jokes.js
+++ assessment-fd/src/pages/Jokes.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import Home from './Home'
 
-function Jokes() {
+export class Jokes extends React.Component {
+    render () {
+    
+        return (
+            <div>
+                <h1>Hello, </h1>
+            </div>
+        );
+    }
+}
+
+/* function Jokes() {
   return (
     <div>Hello, </div>
   )
-}
+} */
 
 export default Jokes
