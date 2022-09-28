a901f664b34a026a1d6d425cf7f8e68262f28e05
Richelle Tan <r.kyj89@gmail.com>
2022-09-29 03:57:01 +0800

    save point 3

--- assessment-fd/src/pages/Jokes.js
+++ assessment-fd/src/pages/Jokes.js
@@ -2,35 +2,47 @@ import * as React from 'react';
 import Navbar from '../components/Navbar'
 
 function Jokes() {
-    React.useEffect(()=>{
+    const urls = [
+        "https://v2.jokeapi.dev/joke/Any?amount=10", 
+        "https://v2.jokeapi.dev/joke/Pun?amount=10",
+        "https://v2.jokeapi.dev/joke/Dark?amount=10",
+        "https://v2.jokeapi.dev/joke/Programming?amount=10",
+      ];
+      
+      async function fetchAll() {
+        const results = await Promise.all(urls.map((url) => fetch(url).then((r) => r.json())));
+        console.log(JSON.stringify(results, null, 2));
+      }
+      
+      fetchAll();
+/*     React.useEffect(()=>{
         Promise.all([
             fetch('https://v2.jokeapi.dev/joke/Any?amount=10'),
             fetch('https://v2.jokeapi.dev/joke/Pun?amount=10'),
             fetch("https://v2.jokeapi.dev/joke/Dark?amount=10"),
             fetch("https://v2.jokeapi.dev/joke/Programming?amount=10"),
         ]).then(function (responses) {
-            // Get a JSON object from each of the responses
+            // get all array from api
             return Promise.all(responses.map(function (response) {
                 return response.json();
             }));
         }).then(function (data) {
-            // Log the data to the console
             // You would do something with both sets of data here
-            console.log(data);
-            console.log(data.jokes)
+            //console.log(data);
         }).catch(function (error) {
-            // show error if any
+            // displays error if any
             console.log(error);
         });
-        },[]) 
+        },[])  */  
 
   return (
     <div>
         <Navbar />
+        
         <h1>
             Jokes List
             <ul>
-
+                
             </ul>
         </h1>
     </div>
