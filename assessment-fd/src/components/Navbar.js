cc94109856ba5b4a7dc34d5670db638e375f60d3
Richelle Tan <r.kyj89@gmail.com>
2022-09-29 01:19:26 +0800

    add popup package and navber update

--- assessment-fd/src/components/Navbar.js
+++ assessment-fd/src/components/Navbar.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import "../styles/Navbar.css";
 
 function Navbar() {
   let userName = localStorage.getItem('username');
 
   return (
-    <div>
-      Hello, {userName}
-    </div>
+<nav className="navbar navbar-light justify-items-between" style={{backgroundColor: "#e3f2fd"}}>
+  <div className="container-fluid">
+    <span className="navbar-brand">Jokes</span>
+    <span className="navbar-brand">Welcome, {userName}!</span>
+  </div>
+</nav>
   )
 }
 
