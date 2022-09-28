8ced06ad49f0630e67ab231953cdae8d115fed48
Richelle Tan <r.kyj89@gmail.com>
2022-09-28 18:46:51 +0800

    redo later on

--- assessment-fd/src/components/jokes.js
+++ assessment-fd/src/components/jokes.js
@@ -1,13 +1,24 @@
 import React from 'react'
+import Header from './header'
 //import Landing from './landing'
+import '../Assets/Styles/jokes.module.css'
+//import { Helmet } from "react-helmet";
+
 const Jokes = () => {
-  return (
-    <div>
-        <h1>
-            Hello
-        </h1>
-    </div>
-  )
+    //const css = require('../Assets/Styles/jokes.css').toString();
+
+    return (
+        <div>
+{/*             <Helmet>
+            <style>
+                {css}
+            </style>
+            </Helmet> */}
+            <h1>
+                Hello 
+            </h1>
+        </div>
+    )
 }
 
 export default Jokes
